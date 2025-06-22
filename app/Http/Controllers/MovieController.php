732bc9a3af113a6a9e93682563e64cbf9c01314c
Novalinaputri <novalinaputri381@gmail.com>
2025-06-23 00:09:49 +0700

    homepage_view

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Movie;
 
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     //
+    public function homepage(){Add commentMore actions
+        //untuk membuat halaman cuma 6 slide
+        $movies = Movie::latest()->paginate(6);
+        return view('homepage', compact('movies'));
+    }
 }
