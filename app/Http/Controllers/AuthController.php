d8390f44e857eebf9fbbfab7e826a97e2b5f9597
Novalinaputri <novalinaputri381@gmail.com>
2025-06-23 02:18:50 +0700

    Make success login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,6 +38,7 @@ class AuthController extends Controller
  
     $request->session()->invalidate(); 
     $request->session()->regenerateToken();
+    
     return redirect('/');
 }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <h1>Latest Movie</h1>
+    @if (session('success'))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        {{session('success')}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    Add commentMore actions
+    @endif
     <div  class="row">
         <!-- ini untuk perulangan 6x -->
         @foreach ($movies as $movie) 
