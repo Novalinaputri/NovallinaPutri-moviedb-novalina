a99e0e87a3c7ffcc1dc056202c7e848b107b15bb
Novalinaputri <novalinaputri381@gmail.com>
2025-06-23 01:03:24 +0700

    Input data

--- app/Models/movie.php
+++ app/Models/movie.php
@@ -10,6 +10,17 @@ class Movie extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'slug',
+        'synopsis',
+        'category_id',
+        'year',
+        'actors',
+        'cover_image',
+    ];
+
+    
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 
 Route::get('/', [MovieController::class, 'homepage'])->name('homepage');
 Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movie.detail');
+Route::get('/create-movie', [MovieController::class, 'create']);
+Route::post('/movie-store', [MovieController::class, 'store']);
